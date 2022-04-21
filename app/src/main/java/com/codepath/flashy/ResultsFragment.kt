68c81861dd7615019ee22d124f069098aadd4b1a
Sumbul Zehra <sumbulzehra.hk@gmail.com>
2022-04-21 12:43:40 -0400

    added quiz results screen

---
+++ app/src/main/java/com/codepath/flashy/ResultsFragment.kt
@@ -0,0 +1,45 @@
+package com.codepath.flashy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.codepath.flashy.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class ResultsFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //button done take to collection activity
+
+        binding.btnDone.setOnClickListener {
+            findNavController().navigate(R.id.action_myCollection)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
